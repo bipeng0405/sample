f6b58a7fed228bc1a03e9a2fc3a3fdd85c1b5feb
vagrant <vagrant@homestead>
2018-02-07 13:44:01 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title','主页')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello BPEX</h1>
+    <p class="lead">
+      你现在所看到的是<a href="">bpex</a>的博客
+    <p>
+    <p>
+      一切，从这里开始
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在开始注册</a>
+    </p>
+  </div>
 @stop
